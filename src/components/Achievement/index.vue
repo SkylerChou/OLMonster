16e7cf8cfb68344f93902b22030f3e27fcbdf5fc
KimBitbucketCloud <kim19961012@gmail.com>
2020-07-15 14:19:56 +0800

    任務完成

--- src/components/Achievement/index.vue
+++ src/components/Achievement/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="container-fluid">
     <div class="all">
+      <h1>任務</h1>
+      <div class="content">
+        <div class="box">
+          <div>
+            <ul>
+              <h2>
+                <img class="check" src="@/assets/svg/check1.svg" v-if="!isFinish[0]" />
+                <img class="check" src="@/assets/svg/check2.svg" v-if="isFinish[0]" />
+                <del v-if="isFinish[0]">學習新知識</del>
+                <span v-if="!isFinish[0]">學習新知識</span>
+              </h2>
+              <h2>
+                <img class="check" src="@/assets/svg/check1.svg" v-if="!isFinish[1]" />
+                <img class="check" src="@/assets/svg/check2.svg" v-if="isFinish[1]" />
+                <del v-if="isFinish[1]">餵怪獸吃飯</del>
+                <span v-if="!isFinish[1]">餵怪獸吃飯</span>
+              </h2>
+            </ul>
+          </div>
+        </div>
+      </div>
       <h1>成就</h1>
       <div class="row">
         <div class="col-4" v-for="(item, index) in srcs" :key="index">
@@ -18,6 +39,7 @@ import cookie from "@/utils/cookie";
 export default {
   data() {
     return {
+      isFinish: [],
       isActive: [],
       srcs: [
         {
@@ -78,6 +100,19 @@ export default {
       .then(res => {
         // console.log(res.data);
         this.isActive = res.data.message;
+      })
+      .catch(function(error) {
+        console.log("請求失敗", error);
+      });
+    this.$axios
+      .get("http://104.199.134.68:8080/user/getmission", {
+        headers: {
+          Authorization: key
+        }
+      })
+      .then(res => {
+        // console.log(res.data);
+        this.isFinish = res.data.message;
       })
       .catch(function(error) {
         console.log("請求失敗", error);
